dc8e101a1f65f7139896428e152669269fa7a87b
neimad971 <damientalbot26@gmail.com>
2020-04-22 14:57:14 +0200

    fixes errors message not showing up on partner/stocks/form
      - adds Stock#presence_of_product_reference and Stock#presence_of_price
    
     Please enter the commit message for your changes. Lines starting

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -1,11 +1,26 @@
 class Stock < ApplicationRecord
 
-  validates :product_reference, presence: true
+  validate :presence_of_product_reference, on: [:create, :update]
+  validate :presence_of_price, on: [:create, :update]
   validates :shop_id, presence: true
 
   belongs_to :product, foreign_key: "product_reference"
   belongs_to :shop
 
   has_many :line_items
+
+  private
+
+    def presence_of_product_reference
+      if product_reference.blank?
+        errors[:base] << "Please select product to add to your shop"
+      end
+    end
+
+    def presence_of_price
+      if price.blank?
+        errors[:base] << "Please choose a price for this product"
+      end
+    end
 
 end
