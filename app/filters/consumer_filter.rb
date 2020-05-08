bf4291c61e3c6e0b07aa2335e83b08145f12e502
neimad971 <damientalbot26@gmail.com>
2020-05-08 04:50:18 +0200

    refactors consumer_filter
      - adds #redirect_with_alert to it

--- app/filters/consumer_filter.rb
+++ app/filters/consumer_filter.rb
@@ -2,33 +2,36 @@ module ConsumerFilter
 
   def require_to_be_consumer
     unless current_user.role.code == Role::CONSUMER_ROLE_CODE
-      flash[:alert] = 'The page you were looking for requires consumer access rights'
-      redirect_to root_path
+      redirect_with_alert("The page you were looking for requires consumer access rights", root_url)
     end
   end
 
   def require_to_be_cart_owner
     id = params[:cart_id] || params[:id]
     unless current_user.cart.id == id.to_i
-      flash[:alert] = "You are not the owner of this cart"
-      redirect_to root_url
+      redirect_with_alert("You are not the owner of this cart", root_url)
     end
   end
 
   def require_to_be_order_owner
     order = Order.find(params[:id])
     unless current_user.id == order.user.id
-      flash[:alert] = "You are not the owner of this order"
-      redirect_to root_url
+      redirect_with_alert("You are not the owner of this order", root_url)
     end
   end
 
   def require_to_be_cart_owner_item
     line_item = LineItem.find(params[:id])
     unless current_user.cart.id == line_item.cart.id
-      flash[:alert] = "This product is not in your cart"
-      redirect_to root_url
+      redirect_with_alert("This product is not in your cart", root_url)
     end
   end
+
+  private
+  
+    def redirect_with_alert(msg, url)
+      flash[:alert] = msg
+      redirect_to url
+    end
 
 end
