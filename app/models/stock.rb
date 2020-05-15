ab60912357423bb598e5a0a2e7866da00e098809
neimad971 <damientalbot26@gmail.com>
2020-05-15 04:03:16 +0200

    Changes how Stock's product was validated so it be simpler
      - replaces custom methods with validates_associated and validates
      - replaces f.select with f.association as select on form
      - updates translation accordingly

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -1,26 +1,12 @@
 class Stock < ApplicationRecord
 
-  validate :presence_of_product_reference, on: [:create, :update]
-  validate :presence_of_price, on: [:create, :update]
+  validates_associated :product, on: [:create, :update]
+  validates :price, presence: true, on: [:create, :update]
   validates :shop_id, presence: true
 
   belongs_to :product, foreign_key: "product_reference"
   belongs_to :shop
 
   has_many :line_items
-
-  private
-
-    def presence_of_product_reference
-      if product_reference.blank?
-        errors[:base] << "#{I18n.t('.blank_product_ref_error')}"
-      end
-    end
-
-    def presence_of_price
-      if price.blank?
-        errors[:base] << "#{I18n.t('.blank_price_error')}"
-      end
-    end
 
 end
