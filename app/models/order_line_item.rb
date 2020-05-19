5d7c750309542b444f3cf3359de803028022eb84
neimad971 <damientalbot26@gmail.com>
2020-05-20 01:06:56 +0200

    adds validations on OrderLineItem model
      - validations for name, unit_price and quantity
      - unit_price and quantity validations might be done over as stocks#price
        is checked as well as LineItem#quantity those attributes are copied
        into a OrderLineItem when placing an order. But I think it worth it
        to check again just in case something goes wrong. Something to think
        about is, in case only one OrderLineItem is not saved
        (happening Order#add_line_item) in the placing order process, should
        I stop the process entirely or keep going in the process without
        having this one and only particular item saved and therefore not
        in the customer's order ?
      - updates order_line_item_spec

--- app/models/order_line_item.rb
+++ app/models/order_line_item.rb
@@ -1,5 +1,9 @@
 class OrderLineItem < ApplicationRecord
 
+  validates :name, presence: true
+  validates :unit_price, presence: true, numericality: { greater_than: 0 }
+  validates :quantity, presence: true, numericality: { only_integer: true, greater_than: 0 }
+
   validates :order_id, presence: true
   validates :shop_id, presence: true
 
--- spec/models/order_line_item_spec.rb
+++ spec/models/order_line_item_spec.rb
@@ -2,6 +2,15 @@ require 'rails_helper'
 
 RSpec.describe OrderLineItem, type: :model do
 
+  it { should validate_presence_of :name }
+  it { should validate_presence_of :unit_price }
+  it { should validate_numericality_of(:unit_price).is_greater_than(0) }
+  it { should validate_presence_of :quantity }
+  it { should validate_numericality_of(:quantity).only_integer.is_greater_than(0) }
+
+  it { should validate_presence_of :order_id }
+  it { should validate_presence_of :shop_id }
+
   it { should belong_to :order }
   it { should belong_to :shop }
 
