3b5237eb4db58a9fd11742c3da1b77410185a38c
neimad971 <damientalbot26@gmail.com>
2020-05-19 23:17:34 +0200

    adds cart_id validation to line_item model + removes optional: true
      - updates line_item_spec

--- app/models/line_item.rb
+++ app/models/line_item.rb
@@ -1,9 +1,10 @@
 class LineItem < ApplicationRecord
 
+  validates :cart_id, presence: true
   validates :stock_id, presence: true
   validates :quantity, presence: true, numericality: { only_integer: true, greater_than: 0 }
 
-  belongs_to :cart, optional: true
+  belongs_to :cart
   belongs_to :stock
 
   def price
--- spec/models/line_item_spec.rb
+++ spec/models/line_item_spec.rb
@@ -2,11 +2,12 @@ require 'rails_helper'
 
 RSpec.describe LineItem, type: :model do
 
+  it { should validate_presence_of :cart_id }
   it { should validate_presence_of :stock_id }
   it { should validate_presence_of(:quantity) }
   it { should validate_numericality_of(:quantity).only_integer.is_greater_than(0) }
 
-  it { should belong_to(:cart).optional }
+  it { should belong_to :cart }
   it { should belong_to :stock }
 
 end
