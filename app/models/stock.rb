da80c6872dd42fba8b2e967b535e6aeaa30ca68c
neimad971 <damientalbot26@gmail.com>
2020-05-19 21:57:45 +0200

    makes sure stock's price is numeric and greater than zero
      - updates stock_spec
      - adds some translations

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -1,7 +1,7 @@
 class Stock < ApplicationRecord
 
   validates_associated :product, on: [:create, :update]
-  validates :price, presence: true, on: [:create, :update]
+  validates :price, presence: true, on: [:create, :update], numericality: { greater_than: 0 }
   validates :shop_id, presence: true
 
   belongs_to :product, foreign_key: "product_reference"
--- spec/models/stock_spec.rb
+++ spec/models/stock_spec.rb
@@ -2,6 +2,8 @@ require 'rails_helper'
 
 RSpec.describe Stock, type: :model do
 
+  it { should validate_presence_of(:price).on(:create).on(:update) }
+  it { should validate_numericality_of(:price).is_greater_than(0) }
   it { should validate_presence_of :shop_id }
 
   it { should belong_to :product }
