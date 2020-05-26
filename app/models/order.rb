dbe0c3a8d2f12c0a14cbf26ea6f08eb12320cbc8
neimad971 <damientalbot26@gmail.com>
2020-05-26 04:23:44 +0200

    updates when outside coverage msg should show up
      - msg show when consumer has placed order which will be delivered
        either to his door or at a parcel pick up and if the shop hasn't
        decided the outside coverage delivery fee yet.
      - updates views with those changes
      - adds mssing validations on Order model
      - adds validations tests for this model
      - updates translations

--- app/models/order.rb
+++ app/models/order.rb
@@ -12,10 +12,12 @@ class Order < ApplicationRecord
   validates :delivery_option_code, presence: true
   validates :status_id, inclusion: { in: STATUS.keys }
   validates :user_id, presence: true
+  validates :outside_shop_coverage_fee, numericality: { greater_than_or_equal_to: 1 }
 
   has_many :order_line_items, dependent: :destroy
   belongs_to :user
   belongs_to :delivery_option, foreign_key: "delivery_option_code"
+
 
   def add_line_item(items)
     items.each do |item|
--- spec/models/order_spec.rb
+++ spec/models/order_spec.rb
@@ -2,6 +2,11 @@ require 'rails_helper'
 
 RSpec.describe Order, type: :model do
 
+  it { should validate_presence_of :delivery_option_code }
+  it { should validate_inclusion_of(:status_id).in_array(Order::STATUS.keys) }
+  it { should validate_presence_of :user_id }
+  it { should validate_numericality_of(:outside_shop_coverage_fee).is_greater_than_or_equal_to(1) }
+
   it { should have_many :order_line_items }
   it { should belong_to :user }
 
