3d33ccb1d7aabe3f6fa561e26bec4b1525c736f6
neimad971 <damientalbot26@gmail.com>
2020-06-03 19:21:07 +0200

    sends shipped order email to customer when order is being delivered to his house
      - adds shipped order email in Partner::orders_controller
      - adds and fixes translations

--- app/mailers/order_mailer.rb
+++ app/mailers/order_mailer.rb
@@ -13,7 +13,15 @@ class OrderMailer < ApplicationMailer
   def cancelled_order
   end
 
-  def shipped_order
+  def shipped_order(order, opts={})
+    if order.status_id == Order::STATUS.key('shipped_status') &&
+      order.delivery_option_code == DeliveryOption::CUSTOMER_PLACE_OPTION_CODE
+      @order = order
+    	mail(to: @order.user.email)
+    	opts[:from] = 'no-reply@example.com'
+      opts[:reply_to] = 'no-reply@example.com'
+      opts[:subject] = 'Your order has been shipped'
+    end
   end
 
 end
