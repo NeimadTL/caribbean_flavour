85e94f6e5f1b2a464ae01d7c9719da84034cc09d
neimad971 <damientalbot26@gmail.com>
2020-06-14 18:44:22 +0200

    shows the right image for a given product
      - adds Product::ICONS hash constant for referencing a product reference
       and his image
      - starts to add some images for some product in the app
      - selects the images via Product::ICONS constant on views

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,6 +2,13 @@ class Product < ApplicationRecord
 
   self.primary_key = 'reference'
 
+  ICONS = {
+    "poyo_ref" => "green_banana.png",
+    "igname_ref" => "yam.jpeg",
+    "banana_ref" => "yellow_banana.png"
+  }
+  ICONS.default = "no_image_available.png"
+
   validates :reference, presence: true, uniqueness: true
   validates :name, presence: true
   validates :product_category_id, presence: true
