42e1d817d20eb308248899c10c395e703ce81f76
neimad971 <damientalbot26@gmail.com>
2020-06-17 20:37:25 +0200

    Defines rate constant in partner::billing_controller

--- app/controllers/partner/billing_controller.rb
+++ app/controllers/partner/billing_controller.rb
@@ -3,6 +3,10 @@ class Partner::BillingController < ApplicationController
   before_action :authenticate_user!
   before_action :require_to_be_partner
   before_action :require_to_have_shop
+  # needs to be called but needs route to be updated first
+  # before_action :require_to_be_shop_owner
+
+  RATE = 3.0 / 100
 
   def index
     @orders = current_user.shop.orders.distinct.order(created_at: :desc)
