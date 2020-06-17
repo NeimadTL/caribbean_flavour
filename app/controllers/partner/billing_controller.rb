fcf52158fd990ef9b57eb4d3518383f625a76a28
neimad971 <damientalbot26@gmail.com>
2020-06-17 22:34:53 +0200

    allows partner to filter billing by month
      - adds months hash constant in Partner::BillingController
      - updates Partner::BillingController#index to take into account filter

--- app/controllers/partner/billing_controller.rb
+++ app/controllers/partner/billing_controller.rb
@@ -8,8 +8,28 @@ class Partner::BillingController < ApplicationController
 
   RATE = 3.0 / 100
 
+  MONTHS = {
+    1 => 'JAN',
+    2 => 'FEB',
+    3 => 'MAR',
+    4 => 'APR',
+    5 => 'MAY',
+    6 => 'JUN',
+    7 => 'JUL',
+    8 => 'AUG',
+    9 => 'SEP',
+    10 => 'OCT',
+    11 => 'NOV',
+    12 => 'DEC'
+  }
+
   def index
     @orders = current_user.shop.orders.distinct.order(created_at: :desc)
+    if params[:month]
+      @orders = @orders.where("extract(month from orders.created_at) = ?", params[:month])
+    else
+      @orders = @orders.where("extract(month from orders.created_at) = ?", Time.now.month)
+    end
   end
 
 end
